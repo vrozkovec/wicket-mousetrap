0474c1c3cc93d2ecfcbb3dbc8f12e26b5d7a3570
Vit Rozkovec <vit.rozkovec@gmail.com>
2019-07-19 14:00:55 +0200

    Update to wicket 8.5.0

--- src/main/java/com/mysticcoders/wicket/mousetrap/MouseTrapGlobalJSReference.java
+++ src/main/java/com/mysticcoders/wicket/mousetrap/MouseTrapGlobalJSReference.java
@@ -8,24 +8,23 @@ import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 /**
- * A resource reference that contributes Mousetrap's
- * <a href="https://github.com/ccampbell/mousetrap/tree/master/plugins/global-bind">Global bind</a>
- * plugin. This extension allows you to specify keyboard events that will work anywhere including inside textarea/input fields.
- * To enable it use {@link com.mysticcoders.wicket.mousetrap.Mousetrap.BindType#GLOBAL} or
- * {@link com.mysticcoders.wicket.mousetrap.Mousetrap.BindType#DEFAULT_GLOBAL} bind types
+ * A resource reference that contributes Mousetrap's <a href=
+ * "https://github.com/ccampbell/mousetrap/tree/master/plugins/global-bind">Global
+ * bind</a> plugin. This extension allows you to specify keyboard events that
+ * will work anywhere including inside textarea/input fields. To enable it use
+ * {@link com.mysticcoders.wicket.mousetrap.Mousetrap.BindType#GLOBAL} or
+ * {@link com.mysticcoders.wicket.mousetrap.Mousetrap.BindType#DEFAULT_GLOBAL}
+ * bind types
  */
-public class MouseTrapGlobalJSReference extends JavaScriptResourceReference
-{
-	public MouseTrapGlobalJSReference()
-	{
+public class MouseTrapGlobalJSReference extends JavaScriptResourceReference {
+	public MouseTrapGlobalJSReference() {
 		super(MouseTrapGlobalJSReference.class, "mousetrap-global-bind.js");
 	}
 
 	@Override
-	public Iterable<? extends HeaderItem> getDependencies()
-	{
-		List<HeaderItem> headerItems = new ArrayList<HeaderItem>();
-		headerItems.add(JavaScriptHeaderItem.forReference(new MouseTrapJSReference()));
-		return headerItems;
+	public List<HeaderItem> getDependencies() {
+		List<HeaderItem> dependencies = super.getDependencies();
+		dependencies.add(JavaScriptHeaderItem.forReference(new MouseTrapJSReference()));
+		return dependencies;
 	}
 }
